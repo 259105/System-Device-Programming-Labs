95e27153d91d477838c599ddd2ec1933a1b8116c
MICHELE CERRA s301117 <s301117@studenti.polito.it>
2022-03-16 19:14:06 +0100

    Added solution with lock of lab2ex2

--- lab2/ex2.c
+++ lab2/ex2.c
@@ -4,9 +4,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-
 #define MAXCHAR 30
+
+int lock_region(int fd, int cmd, short type, short whence, off_t start, off_t len);
 
 typedef struct{
 	int id;
@@ -15,6 +15,15 @@ typedef struct{
 	char surname[MAXCHAR+1];
 	int mark;
 } Student;
+
+int lock_region(int fd, int cmd, short type, short whence, off_t start, off_t len){
+	struct flock flockS;
+	flockS.l_type = type;
+	flockS.l_whence = whence;
+	flockS.l_start = start;
+	flockS.l_len = len;
+	return fcntl(fd,cmd,&flockS);
+}
 
 int main(int argc, char *argv[]){
 	if(argc != 2){
@@ -34,31 +43,30 @@ int main(int argc, char *argv[]){
 	
 	while(1){
 		fprintf(stdout, "Insert command:\n");
-		fflush(stdout);
 		if(fscanf(stdin,"%c %d%*c",&cmd, &n)<=0){
 			fprintf(stderr,"Error: wrong option\n");
 			continue;
 		}
-		fflush(stdin);	
 		if(n == 0) continue;
 		lseek(fd,(n-1)*sizeof(Student),SEEK_SET);
 		switch(cmd){
 		case 'R':
+			lock_region(fd,F_SETLKW,F_RDLCK,SEEK_SET,(n-1)*sizeof(Student),sizeof(Student));
 			rb = read(fd,s,sizeof(Student));
+			lock_region(fd,F_SETLKW,F_UNLCK,SEEK_SET,(n-1)*sizeof(Student),sizeof(Student));
 			if(rb != sizeof(Student) || s->id==0 ){
 				fprintf(stderr,"Error reading the binary file or student doesn't exist\n");
 				break;
 			}
 			fprintf(stdout,"%d %li %s %s %d\n",s->id,s->num,s->name,s->surname,s->mark);
-			fflush(stdout);
 			break;
 		case 'W':
 			fprintf(stdout,"Insert data: <matricola> <name> <surname> <mark>\n");
-			fflush(stdout);
 			s->id = n;
 			fscanf(stdin,"%li %s %s %d%*c",&s->num,s->name,s->surname,&s->mark);
-			fflush(stdin);
+			lock_region(fd,F_SETLKW,F_WRLCK,SEEK_SET,(n-1)*sizeof(Student),sizeof(Student));
 			rb = write(fd,s,sizeof(Student));
+			lock_region(fd,F_SETLKW,F_UNLCK,SEEK_SET,(n-1)*sizeof(Student),sizeof(Student));
 			if(rb != sizeof(Student) ){
 				fprintf(stderr,"Error writing in binary file: %d\n",rb);
 				exit(1);
